705ee0adc91326993c77cf74e7c33924ae2000dc
Aly Abdelrahman <abdelrahmanaly5@gmail.com>
2020-05-17 19:04:32 -0400

    first lesson

---
+++ Tutorials/printing.py
@@ -0,0 +1,19 @@
+#print("hello world") #sample on how to print hello world
+
+#---> integer stands for number i.e 1,2,3,4,5,6,7,8 not 1.1 or 2.1
+#---> double stand for numbers with decimal places 1.1,2.0, 3.05 and has biggest memory storage
+#---> float samething for double more decimal places
+#--->string text i.e "hello, how are you today "
+#---> boolean true or false
+
+#in python we dont have to declare the type of variables that we are using.
+#it is done dynamically for us.
+
+print ("Jesse Plaschka")
+temp= "Jesse Plaschka"
+print (temp[:5]) #string concatination [ignore spelling :(]
+
+# different ways to print
+
+# how to declare variable
+    #temp = 5 or "hello world or 3.01"
